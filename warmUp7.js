78c9f24cf965567a3a0a95717c7a33ca1ad57f05
HamamEL <hamam.alsaiity@gmail.com>
2019-10-24 01:53:08 -0700

    my commit

--- warmUp7.js
+++ warmUp7.js
@@ -4,3 +4,32 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+function add(string){
+	var x = ' ';
+	var name = {
+		firstname : Hamam,
+		lastname  : Elmuratdh
+	}
+	return name.firstname + ' ' + name.lastname;
+}
+function check(number){
+	for (let i = 0; i < number; i ++){
+		if(Number.isInteger(n / 3)){
+			return number;
+		}
+	}		return "number is not multiplay of 3"
+}
+function cal(array){
+	var x = [13,14,13,15,16,17,19,13,16,15];
+	var y [];
+	for (i = 0; i <array.length; i ++){
+		array = array[i] / array.length;
+		array.push(y)
+	}
+
+		return array;
+}
+function myAgeInSec(n){
+	var x = 25;
+	return 25 * 365 * 12 * 24 * 60;
+}
