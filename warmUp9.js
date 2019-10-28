35f60be1d0da8ef74e0969db2090e2c37e3e0443
HamamEL <hamam.alsaiity@gmail.com>
2019-10-28 02:34:53 -0700

    my challenge

--- warmUp9.js
+++ warmUp9.js
@@ -1,10 +1,35 @@
-// Create an HTML file
-// Create a CSS file
-// link the CSS file to the HTML file
-// The page should have a header, body and footer
-// Change the background color of the body to be blue
-// Insert Three images next to each other
-// Create Two buttons and use different font family for each button
-// Create an unordered list with three anchor tags in it
-// Place In the footer a copyright claimer
-// Have Fun
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Presonal HTML page </title>
+	<meta charset="utf-8">
+	<link rel = "stylesheet"
+   type = "text/css"
+   href = "myStyle.css" />
+</head>
+<body>
+<h1> Testing html page </h1>
+
+<div class= "imagesblock">
+				<img src= 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjso-utl7zlAhXB8uAKHS-_AXkQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.thecrazytourist.com%2F15-best-places-visit-libya%2F&psig=AOvVaw0OHrQHNGFb07SM5fbPzI9o&ust=1572256830330332' alt= 'first image'/>
+				<img src= 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwis7-SJmLzlAhWl6uAKHR99DYoQjRx6BAgBEAQ&url=https%3A%2F%2Fforfun.com%2FL0Z0&psig=AOvVaw3U_2epDH6Y35KbV_EDqDOT&ust=1572257041124774' alt= 'secound image'/>
+				<img src= 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwih89mvmLzlAhWP3OAKHaVPBf0QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F802625964813037000%2F&psig=AOvVaw0uV7Oscsg0xyx-RUBUAzen&ust=1572257114284307' alt= 'third image '/>
+			</div>
+			<div class="buttons">
+				<button id= "first button">click here</button>
+				<button id= "secound button">click here</button>
+			</div>
+
+			<ul>
+				<li><a href="https://en.wikipedia.org/wiki/Libya "target="_blank">first link</a></li>
+				<li><a href="https://www.britannica.com/place/Libya" target="_blank">secound link</a></li>
+				<li><a href="https://www.cia.gov/library/publications/the-world-factbook/geos/ly.html" target="_blank">third link</a></li>
+			</ul>
+		</main>
+		<footer>ALL COPY RIGHTS RESERVED Hamam Elmuratdh @2019</footer>
+
+
+</body>
+
+</html>
